9113a23d6c69dbe74392227677418430e0b88cf3
Shoptorshi Jamil <sjami002@gmail.com>
2019-01-23 05:03:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var candyString = "foo";
+var candyString = [];
 
 function addElementToBeginningOfArray(chocolateBars, candyString){
 return [candyString, chocolateBars];
